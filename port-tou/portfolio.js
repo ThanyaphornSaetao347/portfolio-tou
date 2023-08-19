5730837b0b260391997aee8f99b6e1a07110dbd4
ThanyaphornSaetao347 <142294145+ThanyaphornSaetao347@users.noreply.github.com>
2023-08-19 22:30:46 +0700

    Delete port-tou directory

--- port-tou/portfolio.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    const imageContainer = document.querySelector('.image-container');
-    const image = imageContainer.querySelector('img');
-    
-    const containerSize = imageContainer.offsetWidth;
-    image.style.maxWidth = containerSize + 'px';
-});
